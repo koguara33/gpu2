bdb3b71472b5e7c148a0fde31bd4319f3f678d65
koguara33 <108483443+koguara33@users.noreply.github.com>
2022-06-30 15:00:12 -0300

    Update ssh-ngrokok.sh

--- ssh-ngrokok.sh
+++ ssh-ngrokok.sh
@@ -18,14 +18,14 @@ read -p "choose ngrok region: " CRP
 echo "======================="
 echo Updating Please Wait
 echo "======================="
-sudo apt update > /dev/null 2>&1
-sudo apt install openssh-server > /dev/null 2>&1
+apt-get update > /dev/null 2>&1
+apt-get install openssh-server > /dev/null 2>&1
 mkdir -p /var/run/sshd
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc
 echo "export LD_LIBRARY_PATH" >> /root/.bashrc
-sudo service ssh start
+service ssh start
 echo "===================================="
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo create root password
